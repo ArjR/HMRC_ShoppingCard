7c947176a4c37f1de1dc6e79d7d21daf7407b70a
Unknown <arjun.roy@me.com>
2019-02-28 00:19:56 +0000

    Added Checkout Class with implementation

--- ShoppingCart/ShoppingCart/Implementer/Checkout.cs
+++ ShoppingCart/ShoppingCart/Implementer/Checkout.cs
@@ -1,16 +1,74 @@
 ﻿using ShoppingCart.Model;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCart.Implementer
 {
     public class Checkout
     {
-        private List<Item> itemsForSale = new List<Item>();
-        private Dictionary<Item, int> scannedItems = new Dictionary<Item, int>();
+        private List<Item> _itemsForSale = new List<Item>();
+        private Dictionary<Item, int> _scannedItems = new Dictionary<Item, int>();
 
         public Checkout(List<Item> itemsForSale)
         {
-            this.itemsForSale = itemsForSale;
+            _itemsForSale = itemsForSale;
+        }
+
+        public Item IdentifyItem(string itemName)
+        {
+            itemName = itemName.Trim(); // Remove unncessary whitespace
+            return _itemsForSale.FirstOrDefault(x => x.Name.ToLower() == itemName.ToLower() || x.ID.ToString() == itemName);
+        }
+
+        public bool ScanItem(string itemName)
+        {
+            var item = IdentifyItem(itemName);
+
+            if (item == null)
+            {
+                // Item not found
+                return false;
+            }
+            else
+            {
+                // Ensure item is part of scanned items
+                if (!_scannedItems.ContainsKey(item))
+                {
+                    _scannedItems.Add(item, 1);
+                }
+
+                // Add Item to Scanned Items
+                _scannedItems[item]++;
+
+                return true;
+            }
+        }
+
+        public bool ScanItems(string itemList)
+        {
+            var itemStrings = itemList.Split(','); // May need to add extra characters if not comma seperated???
+
+            // Scan each item
+            foreach (var itemString in itemStrings)
+            {
+                var success = ScanItem(itemString);
+
+                if (!success) return false;
+            }
+
+            return true;
+        }
+
+        public decimal CalculateTotal()
+        {
+            var totalPrice = 0M;
+
+            foreach (var itemType in _scannedItems)
+            {
+                totalPrice += itemType.Key.Price * itemType.Value;
+            }
+
+            return totalPrice;
         }
     }
 }
--- ShoppingCart/UnitTestProject1/UnitTest1.cs
+++ ShoppingCart/UnitTestProject1/UnitTest1.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart.Implementer;
+using ShoppingCart.Model;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public Checkout CreateAppleOrangeCheckout()
         {
+            Item apple = new Item();
+            apple.ID = 1;
+            apple.Name = "Apple";
+            apple.Price = 0.60M;
+
+            Item orange = new Item();
+            orange.ID = 2;
+            orange.Name = "Orange";
+            orange.Price = 0.25M;
+
+            var checkout = new Checkout(new List<Item>() { apple, orange });
+
+            return checkout;
         }
     }
 }
