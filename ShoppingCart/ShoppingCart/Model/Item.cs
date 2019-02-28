51914e42883a39d6cc57eb5df05f3f08640dbeb1
Unknown <arjun.roy@me.com>
2019-02-28 00:27:52 +0000

    Added Offer Class to handle shop offers

--- ShoppingCart/ShoppingCart/Model/Item.cs
+++ ShoppingCart/ShoppingCart/Model/Item.cs
@@ -1,9 +1,13 @@
-﻿namespace ShoppingCart.Model
+﻿using System.Collections.Generic;
+
+namespace ShoppingCart.Model
 {
     public class Item
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        public List<Offer> Offers { get; set; } // Maybe multiple offers can be applied?
     }
 }
---
+++ ShoppingCart/ShoppingCart/Model/Offer.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Model
+{
+    public class Offer
+    {
+        public int MinItem { get; set; }
+        public int? AddItem { get; set; }
+        public decimal? DiscountPrice { get; set; } // Total cost of the min items instead
+    }
+}
